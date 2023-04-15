e659a80e1f053a20abb74aab2ed77178aaee8460
ApexPlayground <115173705+ApexPlayground@users.noreply.github.com>
2023-04-15 17:14:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-/*
-  Rock Paper Scissors 🚀🔥
-  Concepts covered in this project
-    👉 For loops
-    👉 Dom Manipulation
-    👉 Variables
-    👉 Conditionals (if else if)
-    👉 Template Literals
-    👉 Event Listeners
-    👉 Higher order Function (Math.random())
-*/
+
 
 //generate a random computer choice
 function getComputerChoice() {
